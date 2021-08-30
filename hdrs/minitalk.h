ccede1a1817a019d1f27eb21e35c369467d7a392
Isaac Donado <idonado@f0r6s8.codam.nl>
2021-08-30 21:16:39 +0200

    basic param checking on client

--- hdrs/minitalk.h
+++ hdrs/minitalk.h
@@ -1,6 +1,7 @@
 #ifndef MINITALK_H
 # define MINITALK_H
-
+# define FALSE 0
+# define TRUE 1
 #include <libft.h>
 #include <ft_printf.h>
 
--- srcs_client/minitalk_client.c
+++ srcs_client/minitalk_client.c
@@ -1,13 +1,32 @@
 #include <minitalk.h>
 
+int is_pid_digit(char *pid)
+{
+    int i;
+
+    i = 0;
+    while (pid[i] != '\0')
+    {
+        if (ft_isdigit(pid[i]) == FALSE)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
     {
-        ft_printf("GTFO of here\n");
-        // TODO error handle here
+        ft_printf("Wrong number of arguements\n\
+The program should be executed as follows;\n./minitalk_client [PID NUMBER] [MESSAGE]\n");
+    return (0);
     }
-    
+    if (is_pid_digit(argv[1]) == FALSE)
+    {
+        ft_printf("PID Number provided is not even a number.\n");
+        return (0);
+    }
     ft_printf("name of program:%s \n", argv[0]);
     return (0);
 }
